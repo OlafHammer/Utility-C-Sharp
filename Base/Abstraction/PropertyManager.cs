102a10083e34ff0ae0465ea8faca39f0d68742f9
OlafHammerfaust <olaf.hammerfaust@gmail.com>
2023-04-03 23:17:54 +0200

    Edit: - Removed Typos - Cleaned up project

--- Base/Abstraction/PropertyManager.cs
+++ Base/Abstraction/PropertyManager.cs
@@ -6,13 +6,13 @@ namespace Utility.Base.Abstraction;
 public static class PropertyManager
 {
     // returns a List of names of all relevant Properties of Object t while ignoring all Properties tagged with the Attribute "HideProperty"
-    public static List<string> PropertyNames(object t, params AvalibleAttributes[] attributes)
+    public static List<string> PropertyNames(object t, params AvailableAttributes[] attributes)
     {
         return EnumProperties(t, attributes).Select(e => e.Name).ToList();
     }
 
     // returns a List of values as T of all relevant Properties of Object t while ignoring all Properties tagged with the Attribute "HideProperty"
-    public static List<T> PropertyValues<T>(object t, params AvalibleAttributes[] attributes) where T : class
+    public static List<T> PropertyValues<T>(object t, params AvailableAttributes[] attributes) where T : class
     {
         List<T> list = new();
         EnumProperties(t, attributes).ToList().ForEach(e =>
@@ -24,18 +24,18 @@ public static class PropertyManager
     }
 
     // returns a List of all relevant Properties of Object t while ignoring all Properties tagged with the Attribute "HideProperty"
-    public static List<PropertyInfo> Properties(object t, params AvalibleAttributes[] attributes)
+    public static List<PropertyInfo> Properties(object t, params AvailableAttributes[] attributes)
     {
         return EnumProperties(t, attributes).ToList();
     }
 
     // returns the amount of all relevant Properties of Object t while ignoring all Properties tagged with the Attribute "HideProperty"
-    public static int PropertyCount(object t, params AvalibleAttributes[] attributes)
+    public static int PropertyCount(object t, params AvailableAttributes[] attributes)
     {
         return EnumProperties(t, attributes).Count();
     }
 
-    private static IEnumerable<PropertyInfo> EnumProperties(object t, IEnumerable<AvalibleAttributes> attributes)
+    private static IEnumerable<PropertyInfo> EnumProperties(object t, IEnumerable<AvailableAttributes> attributes)
     {
         return t.GetType().GetProperties().Where(prop =>
             !prop.CustomAttributes.Any(e =>
